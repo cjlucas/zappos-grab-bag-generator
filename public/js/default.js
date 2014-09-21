395c39bfff1c66fa857a5d6dc190361748e771bd
Chris Lucas <chris@chrisjlucas.com>
2014-09-21 09:32:59 -0700

    tweaks

--- public/js/default.js
+++ public/js/default.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	var el = document.querySelector('button');
 	if (el != null) {
 		el.addEventListener('click', function() {
-			var msg = 'Please be patient while your grab bag is being generated...'
+			var msg = 'Hang on while your grab bag is being generated!'
 			document.querySelector('#flash').textContent = msg;
 		});
 	}
