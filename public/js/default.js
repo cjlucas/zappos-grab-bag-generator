a06fcd7906d6426f999b257effb7dee03e041427
Chris Lucas <chris@chrisjlucas.com>
2014-09-21 09:43:01 -0700

    Added some silliness

--- public/js/default.js
+++ public/js/default.js
@@ -2,7 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
 	var el = document.querySelector('button');
 	if (el != null) {
 		el.addEventListener('click', function() {
-			var msg = 'Hang on while your grab bag is being generated!'
+			var sillyMsgs = [
+				'I hope you have your credit card ready...',
+				'This will be the grab bag to end all grab bags...',
+				'Fingers crossed you get something good...',
+				'Return shipping is always free, but you won\'t need it...'
+			];
+			var silly = sillyMsgs[Math.floor(Math.random() * sillyMsgs.length)];
+			var msg = 'Your grab bag is being generated! ' + silly;
 			document.querySelector('#flash').textContent = msg;
 		});
 	}
